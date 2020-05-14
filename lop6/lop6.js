04bd4c021d8c16c1ae8937a247891dcbd702b548
nguyentu1002 <nguyenvantu1002>
2020-05-15 01:06:44 +0700

    Demmo lớp 6

--- lop6/lop6.js
+++ lop6/lop6.js
@@ -22,6 +22,20 @@ function timSoLienST(){
 }
 //Viết tập hợp bằng cách liệt kê các phần tử
 //Input : 1 <= x <5 Output 1, 2, 3, 4
+function timTHSo(){
+    var dieukien= document.getElementById("tim_thso").value;
+    var arr=dieukien.split('<');
+    var dau= parseInt(arr[0]);
+    var cuoi= parseInt(arr[2]);
+    var th="";
+    if(dau>cuoi)
+        kq+="Nhập khoảng giá trị sai."
+    else{
+        for(var i=dau+1;i<cuoi;i++)
+            th+= i.toString()+ ", ";
+    }
+    document.getElementById("kq_thso").innerHTML= "A = {"+th.substring(0, th.length - 1)+"}";
+}
 
 //Đếm số phần tử
 function demSoPhanTuTapHop(){
@@ -174,7 +188,7 @@ function timBoiN(){
     if(d>c || boi>c)
         kq += "Nhập khoảng giá trị sai. ";
     else{
-        if(boi > d-c)
+        if(boi > c-d)
             kq="Không tồn tại bội nào thõa mãn.";
         else{
             kq += "Nhân "+boi+" lần lượt với các số 0, 1, 2, 3, ... ta được các bội của "+boi +". Trong khoảng "+arr+ " thì tập bội thõa mãn là: ";
@@ -202,18 +216,23 @@ function timUocN(){
 }
 //Hàm tìm n để biểu thức chia hết:
 function tim_SoN(){
-    var kq="";
-    var vt= document.getElementById("vetrai").value;
-    var vp= document.getElementById("vephai").value;
-    var regexObj = new RegExp(/(?:[+\-]|\d[a-z]|\d|[a-z])/g);
-
+    var a1= document.getElementById("a1").value;
+    var a2= document.getElementById("a2").value;
+    var b1= document.getElementById("b1").value;
+    var b2= document.getElementById("b2").value;
+    var kq="Phương pháp giải: ax + b chia hết cho cx+d <br> <=> VT = c.(ax+b) - a(cx+d) chia hết cho cx+d <br> <=> acx + bc - acx - ad chia hết cho cx+d <br> <=> bc - ad chia hết cho cx+d (Tương tự giải như ví dụ trên)";
+    kq += "<br>Ta có: VT = ("+a1+"x +"+b1 +") ⋮ ("+a2+"x +"+b2 +")<br>";
+    kq += " <=> |("+b1+"."+a2+"-"+a1+"."+b2+")|"+"⋮ ("+a2+"x +"+b2+")<br>" ;
+    kq +="<=>"+ (b1*a2 - a1*b2).toString()+"⋮ ("+a2+"x +"+b2+")<br>";
+    var tmp= Math.abs(b1*a2 - a1*b2);
+    var eq;
+    for(var i= 1; i<=tmp;i++){
+        if(tmp%i==0){
+            eq= parseFloat((i-b2)/(a2));
+            kq +=".) "+a2+"x +"+b2+" = "+i+" => x = ("+i+"-" +b2 +") / ("+a2+") = "+ eq +"<br>";
+        }
+    }
     document.getElementById("kq_timson").innerHTML=kq;
-}
-// Hàm tìm x,y thuoc N
-function tim_XY(){
-    var kq="";
-    var ip=document.getElementById("bt_xy").value;
-
 }
 
 //Hàm kiểm tra số nguyên tố
@@ -228,32 +247,121 @@ function kiemTraSoNguyenTo(n){
 }
 //Hàm phân tích thừa số nguyên tố
 function phanTichThuaSoNguyenTo(){
-    var n= document.getElementById("sophantich").value;
+    var n= document.getElementById("pt_snt").value;
     var i= 2;
     var kq="";
-    var tmp;
-    while(n!=1){
-        var dem=0;
-        if(n%i == 0){
+    var dem;
+    for( i=2;i<=n;i++){
+        dem=0;
+        while(n % i == 0){
+            ++dem;
             n /= i;
-            dem++;
-            kq += i.toString()+" * ";
         }
-        else
-            i++;
+        if(dem){
+            if(dem > 1)
+                kq += i+"^"+dem;
+            else
+                kq += i;
+            if(n > i){
+                kq += "*";
+            }
+        }
     }
-    kq=kq.slice(0,kq.length-2);
-    document.getElementById("phantich").innerHTML = kq;
+    document.getElementById("kq_snt").innerHTML = kq;
 }
+//Hàm ứng dụng phân tích thưuà số nguyên tố tìm số ước
+function demUocSNT(){
+    var n= document.getElementById("dem_usnt").value;
+    var tmp=n;
+    var i= 2;
+    var kq="";
+    var dem;
+    var tich=1;
+    for( i=2;i<=n;i++){
+        dem=0;
+        while(n % i == 0){
+            ++dem;
+            n /= i;
+        }
+        if(dem){
+            if(dem > 1)
+                kq += i+"^"+dem;
+            else
+                kq += i;
+            if(n > i){
+                kq += "*";
+            }
+            tich *= dem+1;
+        }
+    }
+    document.getElementById("kq_demusnt").innerHTML =tmp+" = " + kq+"<br> Vậy số ước là : "+tich;
+}
+// //Hàm tìm u c l n
+// function tim_UCLN(){
+//     var u1 = document.getElementById("ucln1").value;
+//     var u2= document.getElementById("ucln2").value;
+//     var i= 2;
+//     var kq="Thừa số chung là : ";
+// //Tách biến thành thừa số
+//     var dem1=0;
+//     let arr_so1 = [];
+//     let arr_dem1= [];
+//     while(u1!=1){       
+//         if(u1%i == 0){
+//             u1 /= i;
+//             dem1++;
+//             if(arr_so1.indexOf(i) < 0){
+//                 arr_so1.push(i);
+//             }
+//         }             
+//         else{ 
+//             i++; 
+//             if(dem1!=0){
+//                 arr_dem1.push(dem1);
+//             }            
+//             dem1=0;
+//         }
+//         if(u1==1){
+//             arr_dem1.push(dem1);
+//         }
+//     }
+//     var dem2=0;
+//     let arr_so2 = [];
+//     let arr_dem2= [];
+//     while(u2!=1){       
+//         if(u2%i == 0){
+//             u2 /= i;
+//             dem2++;
+//             if(arr_so2.indexOf(i) < 0){
+//                 arr_so2.push(i);
+//             }
+//         }             
+//         else{ 
+//             i++; 
+//             if(dem2!=0){
+//                 arr_dem2.push(dem2);
+//             }            
+//             dem2=0;
+//         }
+//         if(u2==1){
+//             arr_dem2.push(dem2);
+//         }
+//     }
+//     var arr= [];
+//     var d=[];
+//     for( var i=0; i< arr_so1.length;i++){
+//         for( var j=0;j<arr_so2.length;j++){
+//             if( arr_so1[i] == arr_so2[j])
+//             {
+//                 arr.push(arr_so1[i]);
+//                 d.push((arr_dem1[i]> arr_dem2[j]) ? arr_dem2[j]: arr_dem1);
+//             }
+//         }
+//     }
+//     for(let i=0;i<arr.length;i++){
+//         kq += `${arr[i]}${d[i]==1?"":"^"+d[i]} * `;
+//     }
+//     kq = kq.slice(0,kq.length-3);
+//     document.getElementById("kq_ucln").innerHTML=kq;
+// }
 
-//Hàm tìm UCLN
-function timUCLN(){
-    
-
-}
-//Hàm tìm BCNN
-function timBCNN(){
-    var x= document.getElementById("boi1").value;
-    var y= document.getElementById("boi2").value;
-    var s
-}
